4da6f5e3fff27379b7ce15edbff5b8339dbc865d
jeka <eugeshka.com@gmail.com>
2026-01-13 12:18:11 +0200

    Corrected counter

--- 2/src/example_package/example_package/example_node.py
+++ 2/src/example_package/example_package/example_node.py
@@ -13,6 +13,7 @@ class SkeletonNode(Node):
         super().__init__('example_node')
         #Create publishers and subscribers in init, use callback
         self.pub = self.create_publisher(String, 'evgenii', 10)
+        self.counter = 0
         self.timer = self.create_timer(0.6, self.publish_msg)
     def publish_msg(self):
         msg = String()
