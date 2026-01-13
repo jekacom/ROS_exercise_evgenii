4d28acc09b4c953e555956ecd9a5a55f07e7c778
jeka <eugeshka.com@gmail.com>
2026-01-13 12:39:22 +0200

    Add subrscriber

--- 3/listener/src/listener_package/listener_package/listener_node.py
+++ 3/listener/src/listener_package/listener_package/listener_node.py
@@ -2,6 +2,7 @@
 import os
 import rclpy
 from rclpy.node import Node
+from std_masgs.msg import String
 
 # Fill in something for msg type imports
 # from duckietown_msgs.msg import SOMETHING
@@ -11,9 +12,10 @@ class ListenerNode(Node):
     def __init__(self):
         super().__init__('listener_node')
         #Create publishers and subscribers in init, use callback
-        pass
+        self.sub = self.create_subscription(String, 'chatter', self.callback_function, 10)
     
-    #Define callback functions here
+    def callback_function(self, msg):
+        self.get_logger().info(f"I heard: {msg.data}")
 
 
 def main():
